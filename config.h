31f74537f148d676848f0ece1190b454c3ceeabb
Boris Wilhelms <mail@wille-zone.de>
2022-02-18 16:11:55 +0100

    Applies status padding.

--- config.h
+++ config.h
@@ -12,6 +12,8 @@ static const int systraypinningfailfirst  = 1;        /* 1: if pinning fails, di
 static const int showsystray              = 1;        /* 0 means no systray */
 static const int showbar                  = 1;        /* 0 means no bar */
 static const int topbar                   = 1;        /* 0 means bottom bar */
+static const int horizpadbar              = 0;        /* horizontal padding for statusbar */
+static const int vertpadbar               = 8;        /* vertical padding for statusbar */
 static const int focusonwheel             = 0;
 static const char *fonts[]                = { "Segoe UI:size=12:antialias=true", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 
