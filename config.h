ad95fdfcd859dce00575f981cac06e1ac28f7fea
boris <mail@wille-zone.de>
2022-07-06 21:14:56 +0200

    Adds grid layout

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
+#include "layouts.c"
 
 /* appearance */
 static const unsigned int borderpx        = 3;        /* border pixel of windows */
@@ -68,6 +69,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "HHH",      grid },	
 };
 
 /* key definitions */
@@ -103,6 +105,7 @@ static Key keys[] = {
 	{ MODKEY,             XK_t,                       setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,             XK_f,                       setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,             XK_m,                       setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,             XK_g,   					  setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,   XK_m,                       spawn,	      SHCMD("sb-mic -t") },
 	{ MODKEY,			  XK_c,                       spawn,	      SHCMD("sb-mic -t") }, // Microphone button on Keychron
 	{ MODKEY,             XK_space,                   setlayout,      {0} },
